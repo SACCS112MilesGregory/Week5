dacc8275271a1e433570733845974001b10ae1cb
Gregory Miles <milesg_99@yahoo.com>
2014-09-23 12:33:35 -0700

    Initial Commit

---
+++ Traingles.java
@@ -0,0 +1,18 @@
+
+public class Traingles 
+{
+
+	public static void main(String[] args) 
+	{
+		int x, y;
+		
+		for(x=1; x<=10; x++)
+		{
+			for(y=0; y<x; y++)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+}
